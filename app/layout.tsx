79473797e45bdf45a9dd04918c9534a9470704b9
jam <chaojin101@gmail.com>
2025-12-25 09:55:06 -0800

    feat(layout): add metadata to root layout

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,5 +1,13 @@
 import { inter } from "@/app/ui/fonts";
 import "@/app/ui/global.css";
+import { Metadata } from "next";
+
+export const metadata: Metadata = {
+  other: {
+    "2dee27924b68014e0a7dc793f7204cc8f7ff25d1":
+      "2dee27924b68014e0a7dc793f7204cc8f7ff25d1",
+  },
+};
 
 export default function RootLayout({
   children,
